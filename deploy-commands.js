5a21f9da671b186d6fecae45ed872ea97938eb6a
Alice <138005616+YashajinAlice@users.noreply.github.com>
2024-08-03 01:29:45 +0800

    更新依赖项 concurrently

---
+++ deploy-commands.js
@@ -0,0 +1,39 @@
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+
+// 遞迴讀取 commands 資料夾中的所有指令檔案
+const loadCommands = (dir) => {
+    const files = fs.readdirSync(dir, { withFileTypes: true });
+    for (const file of files) {
+        if (file.isDirectory()) {
+            loadCommands(path.join(dir, file.name));
+        } else if (file.name.endsWith('.js')) {
+            const command = require(path.join(dir, file.name));
+            commands.push(command.data.toJSON());
+        }
+    }
+};
+
+loadCommands(commandsPath);
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('開始刷新應用程式 (/) 指令。');
+
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: commands },
+        );
+
+        console.log('成功重新載入應用程式 (/) 指令。');
+    } catch (error) {
+        console.error(error);
+    }
+})();
